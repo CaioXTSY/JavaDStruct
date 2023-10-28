6ed90e243127c34074fa713af47b7f8d887fff9c
Caio <125697270+CaioXTSY@users.noreply.github.com>
2023-10-28 12:00:36 -0300

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,55 @@
+public class LinkedList {
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.print();
+        System.out.println("Size of the list: " + list.size());
+    }
+}
